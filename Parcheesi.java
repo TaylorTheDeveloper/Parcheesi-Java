f67a0bce394d66306cae653368baca112f366bdd
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2014-10-27 18:16:01 -0400

    Added some button functionality, extending init. Almost ready to play

--- Parcheesi.java
+++ Parcheesi.java
@@ -5,14 +5,18 @@ import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.Image;
 import java.lang.Exception;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.border.EmptyBorder;
@@ -31,7 +35,15 @@ boardSize-1,boardSize-1 = p4
 public class Parcheesi  extends JFrame implements KeyListener {
 	//movement delay
 	long time = 500;
+	//diceview so user can see what they rolled
+	JLabel diceView;
+	JButton rollDice;
+	JButton restart;
+	JButton quit;
+	//
+	int NUMPLAYERS;
 	int boardSize = 15;
+	int diceValue = 1;
 	JButton board[][];
 
 	public static void main (String[] args){
@@ -41,22 +53,77 @@ public class Parcheesi  extends JFrame implements KeyListener {
 	public Parcheesi(){
 		init();
 		run();
-
 	}
 
-	/* Initilize Game */
+	public void run(){
+		Boolean run = true;
+		int playerCounter = 0;
+		//traverse(0,boardSize-1,3);//p1
+		//traverse(0,0,3);//p2
+		traverse(boardSize-1,0,37);//p3
+		traverse(0,7,37);//p3
+		//traverse(boardSize-1,boardSize-1,5);//p4
+		while(run){
+			//Player Increment
+			playerCounter = (playerCounter+1)%NUMPLAYERS;
+			//RollDice
+			//Traverse
+			//CheckWin
+			//Next Turn
+
+		}
+
+	}
+	/* Initilize Game Board */
 	public void init(){
 		final int MENU_ITEMS = 4;
+		NUMPLAYERS = 0;
 		//Menu Buttons
-		JButton rollDice = new JButton("Roll Dice");
-		JButton restart = new JButton("Start Over");
-		JButton quit = new JButton("Quit");
+		rollDice = new JButton("Roll Dice");
+		restart = new JButton("Start Over");
+		quit = new JButton("Quit");
 		//Frame, panels, etc
 		JFrame frame = new JFrame("Shape Project"); 
 		JPanel contentPane = new JPanel();     
 		board = new JButton [boardSize][boardSize];
 		//Labels
-		JLabel diceView = new JLabel("Roll:");
+		diceView = new JLabel("Roll:");
+		//Dialog for how many players?
+		//JOptionPane optionPane = new JOptionPane("How Many Players?",  JOptionPane.QUESTION_MESSAGE);
+		while(NUMPLAYERS > 4 || NUMPLAYERS < 1){
+			//NUMPLAYERS =0;
+			String[] options = { "1", "2", "3", "4" };
+			String x = (String) JOptionPane.showInputDialog(null, 
+									"How Many Players? (1-4)","Parcheesi",
+									JOptionPane.QUESTION_MESSAGE, 
+									null, options, options[0]); 
+        
+			NUMPLAYERS = Integer.parseInt(x);
+			//
+		}
+
+		//Button Listeners
+		quit.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent e){
+		        System.exit(0);
+		      }
+		    });
+
+		restart.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent e){
+		        init();//Call init to reset the game board, maybe ask user for input
+		      }
+		    });
+
+				//Button Listeners
+		rollDice.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent e){
+		        diceValue = rollDice();
+		        diceView.setText("Roll: " + Integer.toString(diceValue));
+		        rollDice.setEnabled(false);
+		        //Ask which piece to move with dialog
+		      }
+		    });
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100,100,1200,800);
@@ -97,6 +164,12 @@ public class Parcheesi  extends JFrame implements KeyListener {
     	frame.setResizable(true);
 		//frame.setSize(500, 500);
 		frame.setVisible(true);
+	}
+
+	public int rollDice(){
+		Random diceRoller = new Random();
+		int roll = diceRoller.nextInt(12) + 1;
+		return roll;
 	}
 
 
@@ -146,26 +219,26 @@ public void traverse(int x, int y, int roll){
 	//start
 	board[x][y].setBackground(Color.yellow);
 	for(int i = 0; i < roll; i++){
-
-	    	System.out.println(Integer.toString(x) + " " + Integer.toString(y));
+	    //System.out.println(Integer.toString(x) + " " + Integer.toString(y));
 	    if ( y==0 && x<boardSize-1 ) {//good
-	    	System.out.println("x++");
+	    	//System.out.println("x++");
 	        x++;
 	    }
 	    else if (y<boardSize-1 && x==boardSize-1) {
-	    	System.out.println("y++");
+	    	//System.out.println("y++");
 	        y++;
 	    }
 	    else if ( y==boardSize-1 && x>0 ) {
-	    	System.out.println("x--");
+	    	//System.out.println("x--");
 	        x--;
 	    }
 	    else if ( y>0 && x==0 ) {//good
-	    	System.out.println("y--");
+	    	//System.out.println("y--");
 	        y--;
 	    }
 
 	    board[x][y].setBackground(Color.green);
+
 	    //Sleep between movements
 	   	try {
 			Thread.sleep(time);
@@ -177,14 +250,6 @@ public void traverse(int x, int y, int roll){
 	}
 }
 
-	public void run(){
-		//traverse(0,boardSize-1,3);//p1
-		//traverse(0,0,3);//p2
-		traverse(boardSize-1,0,37);//p3
-		traverse(0,7,37);//p3
-		//traverse(boardSize-1,boardSize-1,5);//p4
-
-	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
