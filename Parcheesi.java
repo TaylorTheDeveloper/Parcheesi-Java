1d315473a1a19dca7a60c9f4a59e6afbfd352391
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2014-10-27 16:41:46 -0400

    Added Traversal Algorithm

--- Parcheesi.java
+++ Parcheesi.java
@@ -5,18 +5,32 @@ import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Image;
+import java.lang.Exception;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 import javax.swing.border.EmptyBorder;
+import javax.imageio.ImageIO;
+
+/*
+0,boardSize-1 = p1
+0,0 = p2
+boardSize-1,0 = p3
+boardSize-1,boardSize-1 = p4
+*/
 
 
 
 
 public class Parcheesi  extends JFrame implements KeyListener {
+	//movement delay
+	long time = 500;
 	int boardSize = 15;
 	JButton board[][];
 
@@ -65,7 +79,8 @@ public class Parcheesi  extends JFrame implements KeyListener {
 		panel_menu.add(rollDice);
 		panel_menu.add(restart);
 		panel_menu.add(quit);
-		
+
+
   		//frame.setLayout(new GridLayout(boardSize,boardSize));
 		// //Board
 		for (int y = 0;y<boardSize;y++){
@@ -95,25 +110,79 @@ public void colorSafeZones(){
 	}
 	board[index][index].setBackground(Color.blue);
 	board[index][index].setText("SAFE");
+
+	//Image?
+	try {
+	    Image img = ImageIO.read(getClass().getResource("res/fork.jpg"));
+		//board[index][index].setIcon(new ImageIcon(img));
+	} catch (IOException ex) {
+	}
+	
 }
 
 public void colorLanes(){
-	for(int y = 0; y < boardSize; y++){//Do Row
+	for(int y = 0; y < boardSize; y++){//Left Row
 		board[0][y].setBackground(Color.white);
 	}
-	for(int y = 0; y < boardSize; y++){//Do Row
+	for(int y = 0; y < boardSize; y++){//Right Row
 		board[boardSize-1][y].setBackground(Color.white);
 	}
-	for(int y = 0; y < boardSize; y++){//Do Row
+	for(int y = 0; y < boardSize; y++){//Top Row
 		board[y][0].setBackground(Color.white);
 	}
-	for(int y = 0; y < boardSize; y++){//Do Row
+	for(int y = 0; y < boardSize; y++){//Bottom Row
 		board[y][boardSize-1].setBackground(Color.white);
 	}
 }
+/*
+* x = pos x on board
+* y = pos y on board
+* roll = rollval
+* Issue, wont travers more than 35?
+*/
+public void traverse(int x, int y, int roll){
+	//Traverse Outer Ring
+	
+	//start
+	board[x][y].setBackground(Color.yellow);
+	for(int i = 0; i < roll; i++){
 
+	    	System.out.println(Integer.toString(x) + " " + Integer.toString(y));
+	    if ( y==0 && x<boardSize-1 ) {//good
+	    	System.out.println("x++");
+	        x++;
+	    }
+	    else if (y<boardSize-1 && x==boardSize-1) {
+	    	System.out.println("y++");
+	        y++;
+	    }
+	    else if ( y==boardSize-1 && x>0 ) {
+	    	System.out.println("x--");
+	        x--;
+	    }
+	    else if ( y>0 && x==0 ) {//good
+	    	System.out.println("y--");
+	        y--;
+	    }
+
+	    board[x][y].setBackground(Color.green);
+	    //Sleep between movements
+	   	try {
+			Thread.sleep(time);
+		} 
+		catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
 
 	public void run(){
+		//traverse(0,boardSize-1,3);//p1
+		//traverse(0,0,3);//p2
+		traverse(boardSize-1,0,37);//p3
+		traverse(0,7,37);//p3
+		//traverse(boardSize-1,boardSize-1,5);//p4
 
 	}
 
